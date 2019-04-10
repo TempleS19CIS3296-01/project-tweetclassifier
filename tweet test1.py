c5230ae24e63879bbfeed84e0f2e79558b60ca22
lynx11111 <46969236+lynx11111@users.noreply.github.com>
2019-04-09 23:40:00 -0400

    Add files via upload

---
+++ tweet test1.py
@@ -0,0 +1,43 @@
+import tensorflow as tf
+import pickle
+import numpy as np
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+
+n_nodes_hl1 = 500
+n_nodes_hl2 = 500
+
+n_classes = 2
+
+batch_size = 32
+total_batches = int(1600000/batch_size)
+hm_epochs = 5
+
+x = tf.placeholder('float')
+y = tf.placeholder('float')
+
+
+hidden_1_layer = {'f_fum':n_nodes_hl1,
+                  'weight':tf.Variable(tf.random_normal([2638, n_nodes_hl1])),
+                  'bias':tf.Variable(tf.random_normal([n_nodes_hl1]))}
+
+hidden_2_layer = {'f_fum':n_nodes_hl2,
+                  'weight':tf.Variable(tf.random_normal([n_nodes_hl1, n_nodes_hl2])),
+                  'bias':tf.Variable(tf.random_normal([n_nodes_hl2]))}
+
+output_layer = {'f_fum':None,
+                'weight':tf.Variable(tf.random_normal([n_nodes_hl2, n_classes])),
+                'bias':tf.Variable(tf.random_normal([n_classes])),}
+
+
+
+
+def neural_network_model(data):
+    l1 = tf.add(tf.matmul(data,hidden_1_layer['weight']), hidden_1_layer['bias'])
+    l1 = tf.nn.relu(l1)
+    l2 = tf.add(tf.matmul(l1,hidden_2_layer['weight']), hidden_2_layer['bias'])
+    l2 = tf.nn.relu(l2)
+    output = tf.matmul(l2,output_layer['weight']) + output_layer['bias']
+    return output
