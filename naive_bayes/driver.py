97550b2a11072e40ced97cd30dea0616d291d693
John Mancini <johnmancini30@gmail.com>
2019-04-19 18:11:46 -0400

    changing files

--- naive_bayes/driver.py
+++ naive_bayes/driver.py
@@ -1,5 +1,4 @@
 from parse_data import build_data, extract_features
-from sklearn.naive_bayes import GaussianNB
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score
 
@@ -13,14 +12,11 @@ if __name__=='__main__':
     print("Extracting features")
     train_matrix, train_labels = extract_features(train_dir)
 
-    #model = GaussianNB()
     model = MultinomialNB()
     model.fit(train_matrix, train_labels)
     p_l2 =model.predict(train_matrix)
     print('Accuracy of train predictions:', accuracy_score(train_labels, p_l2) * 100)
 
-    train_matrix = 1
-    train_labels = 1
     
     test_matrix, test_labels = extract_features(test_dir)
 
