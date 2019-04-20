2e620439aef284e690df3ab2fffc59ca342b0165
John Mancini <johnmancini30@gmail.com>
2019-04-20 19:54:22 -0400

    added decision tree folder with some code

---
+++ decision_trees/driver.py
@@ -0,0 +1,27 @@
+from parse_data import build_data, extract_features
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+
+train_dir = "train_data/"
+test_dir = "test_data/"
+
+if __name__=='__main__':
+    print("Building data")
+    build_data(train_dir)
+
+    print("Extracting features")
+    train_matrix, train_labels = extract_features(train_dir)
+
+    model = tree.DecisionTreeClassifier()
+    model.fit(train_matrix, train_labels)
+    p_l2 =model.predict(train_matrix)
+    print('Accuracy of train predictions:', accuracy_score(train_labels, p_l2) * 100)
+
+    
+    test_matrix, test_labels = extract_features(test_dir)
+
+    # Predicting
+    predicted_labels = model.predict(test_matrix)
+
+    print('Accuracy of test predictions:', accuracy_score(test_labels, predicted_labels) * 100)
+
---
+++ decision_trees/parse_data.py
@@ -0,0 +1,61 @@
+import os
+import csv
+from collections import Counter
+import numpy as np
+
+data = {}
+
+MOST_COMMON = 3500 #this is the number of most common words to be analyzed in the model
+delete_symbols = False #a boolean to check if symbols should be deleted from data
+TWEETS_PER_FILE = 100000 #number of tweets in a file
+
+#creates dictionary of most common N words and their corresponding frequency
+def build_data(data_dir):
+    all_words = []
+
+    tweet_files = [data_dir+file for file in os.listdir(data_dir)]
+    #opening and parsing file
+    for file in tweet_files:
+        with open(file, 'r', encoding="ISO-8859-1") as csv_file:
+            reader = csv.reader(csv_file)
+            for row in reader:
+                all_words += row[5].split()
+
+    dictionary = Counter(all_words) #returns counter object of all words
+
+
+    if delete_symbols: #if we want to delete words with symbols and words of length 1
+        list_to_remove = list(dictionary)
+        for word in list_to_remove:
+            if len(word)==1:
+                del dictionary[word] 
+            elif not word.isalpha(): #gets rid of symbols and non word words
+                del dictionary[word]
+                
+    dictionary = dictionary.most_common(MOST_COMMON)#only keep MOST_COMMON words in dictionary
+    count = 0
+    for word in dictionary:
+        data[word[0]] = count
+        count += 1
+
+def extract_features(data_dir):
+    tweet_files = [data_dir+file for file in os.listdir(data_dir)]
+
+    matrix = np.zeros((len(tweet_files)*TWEETS_PER_FILE, MOST_COMMON))
+    labels = np.zeros(len(tweet_files)*TWEETS_PER_FILE)
+
+
+    tweet_count = 0
+
+    for file in tweet_files:
+        with open(file, 'r', encoding="ISO-8859-1") as csv_file:
+            reader = csv.reader(csv_file)
+            for i, row in enumerate(reader, start = 1):
+                line = row[5].split()
+                for word in line:
+                    if word in data:
+                        matrix[tweet_count, data[word]] = line.count(word)
+                labels[tweet_count] = int(row[0])
+                tweet_count+=1 
+    return matrix, labels  
+
