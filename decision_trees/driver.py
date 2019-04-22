f2ea0d7511c55edef7cb04a8b5ae8b4204cefadd
John Mancini <johnmancini30@gmail.com>
2019-04-21 20:46:35 -0400

    going back to decision trees

--- decision_trees/driver.py
+++ decision_trees/driver.py
@@ -1,7 +1,6 @@
 from parse_data import build_data, extract_features
 from sklearn import tree
 from sklearn.metrics import accuracy_score
-from sklearn import svm
 
 train_dir = "train_data/"
 test_dir = "test_data/"
@@ -13,8 +12,7 @@ if __name__=='__main__':
     print("Extracting features")
     train_matrix, train_labels = extract_features(train_dir)
 
-    model = svm.SVC(gamma='scale')
-   # model = tree.DecisionTreeClassifier()
+    model = tree.DecisionTreeClassifier()
     model.fit(train_matrix, train_labels)
     p_l2 =model.predict(train_matrix)
     print('Accuracy of train predictions:', accuracy_score(train_labels, p_l2) * 100)
