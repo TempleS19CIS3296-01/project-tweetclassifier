d778e14a4ed6bdce29384c60f7af861d6b909226
johnmancini123 <46969430+johnmancini123@users.noreply.github.com>
2019-04-24 10:23:06 -0400

    Update driver.py
    adding ability for user to make predicitons

--- decision_trees/driver.py
+++ decision_trees/driver.py
@@ -26,3 +26,23 @@ if __name__=='__main__':
 
     print('Accuracy of test predictions:', accuracy_score(test_labels, predicted_labels) * 100)
 
+
+    #making prediction on new data
+    answer = 1
+    while answer:
+        num_tweets = get_all_tweets()
+        to_predict_matrix, to_predict_tweets = extract_features(predict_dir, 3500, num_tweets)
+        prediction_labels_new_tweets = model.predict(to_predict_matrix)    
+        pos = 0
+        neg = 0
+        for label in prediction_labels_new_tweets:
+            if label == 0:
+                neg += 1
+            elif label == 4:
+                pos+=1
+            else:
+                print("Error")
+        print("Percentage of last", num_tweets, "tweets that were positive:", pos/num_tweets)
+        print("Percentage of last", num_tweets, "tweets that were negative:", neg/num_tweets) 
+        answer = int(input("Would you like to test a different twitter user?"))
+    
